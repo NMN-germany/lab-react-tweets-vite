d24d7e67cddccc85e19f01f40a9cadc653427668
Noemí Mira <noemi.miranavarro@gmail.com>
2025-06-11 23:34:19 +0200

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,33 +1,25 @@
 import ProfileImage from "./ProfileImage";
 import User from "./User";
-import tweetsArray from "./"
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
 
-function Tweet() {
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <ProfileImage image={"https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"} />
+      <ProfileImage image={tweet.user.image}/>
 
       <div className="body">
         <div className="top">            
-          <User name={tweetsArray.user.name} handle={Tweet.user.handle} />
-          
-          <span className="timestamp">1h ago</span>
+          <User name={tweet.user.name} handle={tweet.user.handle} />          
+          <Timestamp time={tweet.timestamp} />
         </div>
 
-        <p className="message">
-          the human likes to say. that i live here rent free. but i would argue.this 
-          housing accommodation. is my payment. for a lifetime of love. and 
-          excellent company
-        </p>
+        <Message message={tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Actions />
       </div>
+
 
       <i className="fas fa-ellipsis-h"></i>
     </div>
