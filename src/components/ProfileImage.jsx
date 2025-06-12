a4674e3414c597ef9d53c407d284cc92f8c792e8
Noemí Mira <noemi.miranavarro@gmail.com>
2025-06-12 10:46:31 +0200

    add again

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,4 +1,4 @@
-function ProfileImage () {
-    return <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg" className="profile" alt="profile" />
+function ProfileImage ( {image}) {
+    return <img src={image} className="profile" alt="profile" />;
 }
 export default ProfileImage;
